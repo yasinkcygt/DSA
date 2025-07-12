7980e21e8ca665a2fd02a5e6be07bea1ba8f902e
Yasin <Yasinkygt@gmail.com>
2025-07-12 15:31:54 +0300

    Implement bubble sort with complex array test

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,16 +1,19 @@
 def bubbleSortAlgorithm(arr):
+    # bubble sort loop
     for i in range(len(arr)):
-        for j in range (0, len(arr)-i-1):
+        for j in range(0, len(arr)-i-1):
+            # swap if current > next
             if arr[j] > arr[j+1]:
                 temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
-    
     return arr
 
-
 def printArray():
+    # input list
     bubble_list = bubbleSortAlgorithm([34, -2, 0, 99, 34, -45, 0, 23, -2, 88, 1000, -999, 23, 0])
+    
+    # print each item
     for i in range(len(bubble_list)):
         print(bubble_list[i])
 
